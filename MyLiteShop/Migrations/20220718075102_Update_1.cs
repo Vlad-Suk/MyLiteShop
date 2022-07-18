b4efcadf52f9b95afcdc31dfda9b314da3cb9ebc
Vlad-Suk <sukvladik00@gmail.com>
2022-07-18 10:56:36 +0300

    Updated model

---
+++ MyLiteShop/Migrations/20220718075102_Update_1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyLiteShop.Migrations
+{
+    public partial class Update_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "numeric(8,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberOfProducts",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(8,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberOfProducts",
+                table: "Products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
--- MyLiteShop/Models/Product.cs
+++ MyLiteShop/Models/Product.cs
@@ -1,11 +1,15 @@
-﻿namespace MyLiteShop.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLiteShop.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public int? Price { get; set; }
-        public int? NumberOfProducts { get; set; } 
+        public string Name { get; set; } = String.Empty;
+        
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public int NumberOfProducts { get; set; } 
         public string? Description { get; set; }
     }
 }
